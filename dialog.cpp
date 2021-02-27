0b0818506993336f062973e1147486984288973a
mateuszbaszaraba <64064788+mateuszbaszaraba@users.noreply.github.com>
2021-02-27 16:07:54 +0100

    basic done

--- dialog.cpp
+++ dialog.cpp
@@ -1,15 +1,141 @@
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <QHBoxLayout>
+#include <QFormLayout>
+#include <QComboBox>
+#include <QSpinBox>
 
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
-    , ui(new Ui::Dialog)
 {
-    ui->setupUi(this);
+    createMenu();
+    createHorizontalGroupBox();
+    createGridGroupBox();
+    createFormGroupBox();
+
+    bigEditor = new QTextEdit;
+    bigEditor->setPlainText(tr("Another cool widget"));
+    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->setMenuBar(menuBar);
+    mainLayout->addWidget(horizontalGroupBox);
+    mainLayout->addWidget(gridGroupBox);
+    mainLayout->addWidget(formGroupBox);
+    mainLayout->addWidget(bigEditor);
+    mainLayout->addWidget(buttonBox);
+
+    setLayout(mainLayout);
+    setWindowTitle(tr("Basic Layoutss"));
 }
 
-Dialog::~Dialog()
+void Dialog::createMenu()
 {
-    delete ui;
+    menuBar = new QMenuBar;
+
+    fileMenu = new QMenu(tr("&File"), this);
+    exitAction = fileMenu->addAction(tr("E&xit"));
+    menuBar->addMenu(fileMenu);
+
+    connect(exitAction, &QAction::triggered, this, &QDialog::accept);
 }
+
+void Dialog::createHorizontalGroupBox()
+{
+    horizontalGroupBox = new QGroupBox(tr("Horizontal Layout"));
+    QHBoxLayout *layout = new QHBoxLayout;
+
+    for(int i=0; i<NumButtons; ++i)
+    {
+        buttons[i] = new QPushButton(tr("Button %1").arg(i+1));
+        layout->addWidget(buttons[i]);
+    }
+    horizontalGroupBox->setLayout(layout);
+}
+
+void Dialog::createGridGroupBox()
+{
+    gridGroupBox = new QGroupBox(tr("Grid layout"));
+    QGridLayout *layout = new QGridLayout;
+
+    for(int i=0; i<NumGridRows; ++i)
+    {
+        labels[i] = new QLabel(tr("Line %1:").arg(i+1));
+        lineEdits[i] = new QLineEdit;
+        layout->addWidget(labels[i], i+1, 0);
+        layout->addWidget(lineEdits[i], i+1, 1);
+    }
+
+    smallEditor = new QTextEdit;
+    smallEditor->setPlainText(tr("Cool widget"));
+
+    layout->addWidget(smallEditor, 0, 2, 4, 1);
+
+    layout->setColumnStretch(1, 10);
+    layout->setColumnStretch(2, 20);
+    gridGroupBox->setLayout(layout);
+}
+
+void Dialog::createFormGroupBox()
+{
+    formGroupBox = new QGroupBox(tr("Form layout"));
+    QFormLayout *layout = new QFormLayout;
+    layout->addRow(new QLabel(tr("Line 1:")), new QLineEdit);
+    layout->addRow(new QLabel(tr("Line 2:")), new QComboBox);
+    layout->addRow(new QLabel(tr("Line 3:")), new QSpinBox);
+    formGroupBox->setLayout(layout);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- dialog.h
+++ dialog.h
@@ -2,6 +2,13 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QMenuBar>
+#include <QGroupBox>
+#include <QTextEdit>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QDialogButtonBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Dialog; }
@@ -13,9 +20,28 @@ class Dialog : public QDialog
 
 public:
     Dialog(QWidget *parent = nullptr);
-    ~Dialog();
 
 private:
-    Ui::Dialog *ui;
+    void createMenu();
+    void createHorizontalGroupBox();
+    void createGridGroupBox();
+    void createFormGroupBox();
+
+    enum { NumGridRows = 3, NumButtons = 4 };
+
+    QMenuBar *menuBar;
+    QGroupBox *horizontalGroupBox;
+    QGroupBox *gridGroupBox;
+    QGroupBox *formGroupBox;
+    QTextEdit *smallEditor;
+    QTextEdit *bigEditor;
+    QLabel *labels[NumGridRows];
+    QLineEdit *lineEdits[NumGridRows];
+    QPushButton *buttons[NumButtons];
+    QDialogButtonBox *buttonBox;
+
+    QMenu *fileMenu;
+    QAction *exitAction;
+
 };
 #endif // DIALOG_H
